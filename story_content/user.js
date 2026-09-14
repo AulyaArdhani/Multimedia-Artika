4f5fe8314e0993915cafce89e4a466dbfa2891b1
awanganggara13-sys <aulyaardhn19@gmail.com>
2026-09-14 17:45:00 +0700

    upload

---
+++ story_content/user.js
@@ -0,0 +1,70 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6BRe0y02i0d":
+        Script1();
+        break;
+      case "60xGXmu9CCa":
+        Script2();
+        break;
+      case "63NoGXBosXC":
+        Script3();
+        break;
+      case "6Xwr7ar26Cc":
+        Script4();
+        break;
+      case "6q3hn1bIydz":
+        Script5();
+        break;
+      case "5oBnB1zsU53":
+        Script6();
+        break;
+      case "6eRvnfTnP10":
+        Script7();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.0;
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.1;
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.2;
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.3;
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.4;
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.5;
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.5;
+}
+
